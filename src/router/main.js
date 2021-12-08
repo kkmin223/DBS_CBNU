7b8ed4fdb78cf46930f82980014c35ac66baa1e6
Repaion24 <repaion24@naver.com>
2021-12-09 00:45:13 +0900

    장바구니, 구매, 구매내역, 쿼리문 수정
    장바구니 기능 구현(겹치면 문제발생)
    구매/구매내역 기능 구현(겹치면 문제발생)
    쿼리문 일부 수정
    (6,7번째 table %_no 부분 Auto inc)
    (구매내역 table date 자동으로 저장)

--- src/router/main.js
+++ src/router/main.js
@@ -4,8 +4,13 @@ const router = express.Router();
 const index_view = require('../views/index')
 const shop_view = require('../views/shop')
 const shop_category_view = require('../views/shop_category')
+const cart_view = require('../views/cart')
+const cart_plus_view = require('../views/cartplus')
+const cart_purchase_view = require('../views/cartpurchase')
+const cart_remove_view = require('../views/cartremove')
 const login_user_view = require('../views/login_user')
 const url = require('url');
+const store = require('store');
 
 
 router.get('/', (req,res)=>{
@@ -29,9 +34,34 @@ router.get('/shop', (req,res) =>{
           let html = shop_view.HTML(game_list)
           res.end(html)
       });
-  } catch(err) {
+   } catch(err) {
+   }
+});
 
-  }
+router.get('/cartplus', (req,res) =>{
+   const {user_id, company_id, game_name, amount} = url.parse(req.url,true).query;
+   try{
+      db.query(`INSERT INTO cart (user_id, company_id, game_name, amount) VALUES (?,?,?,?)`, [user_id, company_id, game_name, amount], (err)=>{
+          if(err) throw new Error(err);
+          let html = cart_plus_view.HTML()
+          res.end(html)
+      });
+   } catch(err) {
+      res.redirect('/')
+   }
+});
+
+router.get('/cartremove', (req,res) =>{
+   const {user_id, company_id, game_name} = url.parse(req.url,true).query;
+   try{
+      db.query(`Delete From cart Where cart.user_id = ? and cart.company_id = ? and cart.game_name = ?`, [user_id, company_id, game_name], (err)=>{
+          if(err) throw new Error(err);
+          let html = cart_remove_view.HTML()
+          res.end(html)
+      });
+   } catch(err) {
+      res.redirect('/')
+   }
 });
 
 router.get('/shop_category', (req,res) =>{
@@ -46,15 +76,57 @@ router.get('/shop_category', (req,res) =>{
           res.end(html)
       });
   } catch(err) {
-     
   }
 });
 
-
-
-router.get('/cart',function(req,res){
-   res.render('cart.html');
+router.get('/cartpurchase', (req,res) =>{
+   const {user_id} = url.parse(req.url,true).query;
+   try{
+      db.query(`Select * From cart Where cart.user_id = ?`, [user_id], (err, number)=>{
+         if(err) throw new Error(err);
+         db.query(`Select user.email From user Where user.id = ?`, [user_id], (err, email)=>{
+            if(err) throw new Error(err);
+            db.query(`Select * From cart Where cart.user_id = ?`, [user_id], (err, games)=>{
+               if(err) throw new Error(err);
+               for(let i=0; i<number.length; i++){
+                  db.query(`INSERT INTO gameorder (user_id, company_id, game_name, email, amount) VALUES (?,?,?,?,?)`, [games[i].user_id, games[i].company_id, games[i].game_name, email, games[i].amount], (err)=>{
+                     if(err) throw new Error(err);
+                  });
+               }
+               db.query(`Delete From cart Where cart.user_id = ?`, [user_id], (err)=>{
+                  if(err) throw new Error(err);
+              });
+            });
+         });
+         let html = cart_purchase_view.HTML()
+         res.end(html)
+      });
+   } catch(err) {
+      res.redirect('/')
+   }
 });
+
+
+
+router.get('/cart', (req,res) =>{
+   const {user_id} = url.parse(req.url,true).query;
+   try{
+      db.query(`SELECT * FROM game WHERE game.name IN (select cart.game_name From cart where cart.user_id = ?)`, [user_id], (err, game)=>{
+          if(err) throw new Error(err);
+          let game_list = cart_view.game_list(game)
+          let summary = cart_view.summary(game)
+          let html = cart_view.HTML(game_list,summary)
+          res.end(html)
+      });
+   } catch(err) {
+      res.redirect('/login_user')
+   }
+});
+
+
+
+
+
 
 router.get('/login_user',function(req,res){
    let html = login_user_view.HTML()
